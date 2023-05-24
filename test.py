ca323774a5eb92e69092fbd6fb004a06f9a84d37
fakhareldin abakar <39832576+wadshatta@users.noreply.github.com>
2023-05-24 23:16:39 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,39 @@
-import pytest
-from flask import jsonify
+import unittest
+from flask import Flask, jsonify
+
+# Import the Flask application and books data
 from app import app, books
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
 
-def test_home(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.data == b'Welcome to the Book API'
+class FlaskAppTests(unittest.TestCase):
 
-def test_get_books(client):
-    response = client.get('/books')
-    assert response.status_code == 200
-    assert response.get_json() == books
+    def setUp(self):
+        # Create a test client
+        self.client = app.test_client()
 
-def test_get_book(client):
-    book_id = 1
-    response = client.get(f'/books/{book_id}')
-    assert response.status_code == 200
-    expected_book = next((book for book in books if book['id'] == book_id), None)
-    assert response.get_json() == expected_book
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Welcome to the Book API')
 
-def test_get_nonexistent_book(client):
-    book_id = 99
-    response = client.get(f'/books/{book_id}')
-    assert response.status_code == 404
-    assert response.get_json() == {'error': 'Book not found'}
+    def test_get_books(self):
+        response = self.client.get('/books')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), books)
+
+    def test_get_book(self):
+        book_id = 1
+        response = self.client.get(f'/books/{book_id}')
+        self.assertEqual(response.status_code, 200)
+        expected_book = next((book for book in books if book['id'] == book_id), None)
+        self.assertEqual(response.get_json(), expected_book)
+
+    def test_get_nonexistent_book(self):
+        book_id = 99
+        response = self.client.get(f'/books/{book_id}')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.get_json(), {'error': 'Book not found'})
+
+
+if __name__ == '__main__':
+    unittest.main()
